086d0e38e46a58e1332aadc90d650c1b9bd964cb
Kit Trowbridge <kittrow@gmail.com>
2019-04-03 14:32:59 +0100

    stil trying js for random generate

--- js-main.js
+++ js-main.js
@@ -1,4 +1,24 @@
-button.onclick = function "My Function"(){
+/* button.onclick = function "My Function"(){
   var myArray = ['January', 'February', 'March'];
   var rand = myArray[Math.floor(Math.random() * myArray.length)];
 }
+*/
+
+function myFunction(){
+  var myArray = ['January', 'February', 'March'];
+var rand = myArray[(Math.random() * myArray.length) | 0]
+
+}
+
+document.getElementById("demo").onclick = function(){myFunction()};
+
+/*function myFunction(){
+  var myArray = ['January', 'February', 'March'];
+  var rand = myArray[Math.floor(Math.random() * myArray.length)];
+}
+*/
+
+/*function myFunction(){
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+*/
